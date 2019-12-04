7c81b567e7c1030a8b0328dca341c44c41c54637
Matthew <matt@matthewpetela.com>
2019-12-04 07:31:21 +0000

    makefile and c chell

---
+++ Makefile
@@ -0,0 +1,2 @@
+make:
+	gcc chompdrv.c -o chompdrv -I/usr/include/libusb-1.0 -lusb-1.0
---
+++ chompdrv.c
@@ -0,0 +1,11 @@
+#include <libusb.h>
+#include <linux/uinput.h>
+
+
+
+int main(int argc, char *argv[]){
+	int fd;
+	
+
+
+}
